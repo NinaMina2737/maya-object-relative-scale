f32e8b67da31e6db39c527f488c0290c91ddc32f
NinaMina2737 <87418579+NinaMina2737@users.noreply.github.com>
2023-05-21 18:01:21 +0900

    Update maya_object_relative_scale_ui.py

--- src/maya_object_relative_scale_ui.py
+++ src/maya_object_relative_scale_ui.py
@@ -21,76 +21,73 @@ class ScaleObjectsUI(MayaQWidgetBaseMixin, QtWidgets.QWidget):
 
         self.setWindowTitle(WINDOW_TITLE)
 
-        self.setupUI(self)
+        self.create_widget()
+        self.create_layout()
 
-        self.vertical_layout = QtWidgets.QVBoxLayout(self)
-        self.vertical_layout.addWidget(self.label_requirement)
-        self.vertical_layout.addWidget(self.scale_freeze_button)
-        self.vertical_layout.addWidget(self.radioGroupBox)
-        self.vertical_layout.addWidget(self.groupBox)
-        self.vertical_layout.addWidget(self.run_button)
+        self.freeze_scale_button.clicked.connect(mors.freeze_scale)
+        self.scale_button.clicked.connect(self.scale)
 
-        self.scale_freeze_button.clicked.connect(mors.scale_freeze)
-        self.run_button.clicked.connect(self.scale)
-
-    def setupUI(self, Form):
-        if not Form.objectName():
-            Form.setObjectName("Form")
-        Form.setWindowTitle("Form")
-        Form.resize(304, 188)
-        self.verticalLayout = QtWidgets.QVBoxLayout(Form)
-        self.verticalLayout.setObjectName("verticalLayout")
-
-        self.requirement_label = QtWidgets.QLabel(Form)
+    def create_widget(self):
+        self.requirement_label = QtWidgets.QLabel()
         self.requirement_label.setObjectName("requirement_label")
         self.requirement_label.setText("Requirement: Select objects to scale.")
         self.requirement_label.setAlignment(QtCore.Qt.AlignCenter)
-        self.verticalLayout.addWidget(self.requirement_label)
 
-        self.freeze_scale_button = QtWidgets.QPushButton(Form)
+        self.freeze_scale_button = QtWidgets.QPushButton()
         self.freeze_scale_button.setObjectName("freeze_scale_button")
         self.freeze_scale_button.setText("Freeze Scale")
-        self.verticalLayout.addWidget(self.freeze_scale_button)
 
-        self.radioGroupBox = QtWidgets.QGroupBox("Axis Options", Form)
-        self.verticalLayout.addWidget(self.radioGroupBox)
+        self.radioGroupBox = QtWidgets.QGroupBox("Axis Options")
 
-        self.horizontalLayout = QtWidgets.QHBoxLayout(self.radioGroupBox)
-
-        self.radioButton_x = QtWidgets.QRadioButton(Form)
+        self.radioButton_x = QtWidgets.QRadioButton()
         self.radioButton_x.setObjectName("radioButton_x")
         self.radioButton_x.setText("X")
         self.radioButton_x.setChecked(True)
-        self.horizontalLayout.addWidget(self.radioButton_x)
 
-        self.radioButton_y = QtWidgets.QRadioButton(Form)
+        self.radioButton_y = QtWidgets.QRadioButton()
         self.radioButton_y.setObjectName("radioButton_y")
         self.radioButton_y.setText("Y")
-        self.horizontalLayout.addWidget(self.radioButton_y)
 
-        self.radioButton_z = QtWidgets.QRadioButton(Form)
+        self.radioButton_z = QtWidgets.QRadioButton()
         self.radioButton_z.setObjectName("radioButton_z")
         self.radioButton_z.setText("Z")
-        self.horizontalLayout.addWidget(self.radioButton_z)
 
-        self.groupBox = QtWidgets.QGroupBox("Scale Options", Form)
-        self.verticalLayout.addWidget(self.groupBox)
+        self.length_groupBox = QtWidgets.QGroupBox("Scale Options")
 
-        self.formLayout = QtWidgets.QFormLayout(self.groupBox)
-        self.formLayout.setLabelAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
-        self.formLayout.setFormAlignment(QtCore.Qt.AlignCenter)
-
-        self.length_doubleSpinBox = QtWidgets.QDoubleSpinBox(Form)
+        self.length_doubleSpinBox = QtWidgets.QDoubleSpinBox()
         self.length_doubleSpinBox.setObjectName("length_doubleSpinBox")
         self.length_doubleSpinBox.setMinimum(-1000.0)
         self.length_doubleSpinBox.setMaximum(1000.0)
         self.length_doubleSpinBox.setValue(1.0)
-        self.formLayout.addRow("", self.length_doubleSpinBox)
+        self.length_doubleSpinBox.setSuffix(" cm")
 
-        self.scale_button = QtWidgets.QPushButton(Form)
+        self.scale_button = QtWidgets.QPushButton()
         self.scale_button.setObjectName("scale_button")
         self.scale_button.setText("Scale")
-        self.verticalLayout.addWidget(self.scale_button)
+
+    def create_layout(self):
+        verticalLayout = QtWidgets.QVBoxLayout(self)
+        verticalLayout.setObjectName("verticalLayout")
+        verticalLayout.addWidget(self.requirement_label)
+        verticalLayout.addWidget(self.freeze_scale_button)
+        verticalLayout.addWidget(self.radioGroupBox)
+
+        horizontalLayout = QtWidgets.QHBoxLayout(self)
+        horizontalLayout.setObjectName("horizontalLayout")
+        horizontalLayout.addWidget(self.radioButton_x)
+        horizontalLayout.addWidget(self.radioButton_y)
+        horizontalLayout.addWidget(self.radioButton_z)
+        self.radioGroupBox.setLayout(horizontalLayout)
+
+        self.formLayout = QtWidgets.QFormLayout(self.length_groupBox)
+        self.formLayout.setLabelAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
+        self.formLayout.setFormAlignment(QtCore.Qt.AlignCenter)
+        self.formLayout.addRow("Length", self.length_doubleSpinBox)
+
+        verticalLayout.addWidget(self.length_groupBox)
+        verticalLayout.addWidget(self.scale_button)
+
+        self.setLayout(verticalLayout)
 
     def scale(self):
         base_axis = None
@@ -113,7 +110,7 @@ class ScaleObjectsUI(MayaQWidgetBaseMixin, QtWidgets.QWidget):
         print("Specified axis:{0}".format(base_axis))
         print("Specified length:{0}".format(length_value))
 
-        mors.scale(base_axis, length_value)
+        mors.execute(base_axis, length_value)
 
 def execute():
     """
