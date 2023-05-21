0b7c1fa6f2a11944b5e238395c2fb802f5400155
NinaMina2737 <87418579+NinaMina2737@users.noreply.github.com>
2023-05-21 18:01:07 +0900

    Update maya_object_relative_scale.py

--- src/maya_object_relative_scale.py
+++ src/maya_object_relative_scale.py
@@ -3,6 +3,8 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import traceback
+
 import maya.cmds as cmds
 
 
@@ -45,9 +47,29 @@ def scale(base_axis, length_value):
         object_lengths = object_lengths_list[i]
         magnifications.append(length_value / object_lengths[axes_mapping[base_axis]])
 
-    for i, object, magnification in enumerate(zip(objects, magnifications)):
+    for i, (object, magnification) in enumerate(zip(objects, magnifications)):
         scale_values = get_scale_values(object)
         for axis, value in zip(axes, scale_values):
             set_scale_value(object, axis, value * magnification)
 
     print('"{0}" axis width is now "{1}" cm.'.format(base_axis.upper(), length_value))
+
+def execute(base_axis, length_value):
+    try:
+        # Open an undo chunk
+        cmds.undoInfo(openChunk=True)
+        # Scale
+        scale(base_axis, length_value)
+    except Exception as e:
+        # Print the error message
+        cmds.warning("An error occurred: {}".format(str(e)))
+        # Print the traceback
+        cmds.warning(traceback.format_exc())
+    finally:
+        # Close the undo chunk
+        cmds.undoInfo(closeChunk=True)
+
+if __name__ == '__main__':
+    # Execute the script
+    execute(base_axis="x", length_value=1.00)
+
